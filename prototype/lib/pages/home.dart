1ea06484270db44e0fade3b4614212c1924242f6
Manmeet <manmeetkohli1049@gmail.com>
2021-11-14 18:12:25 +0530

    profile_icon

--- prototype/lib/pages/home.dart
+++ prototype/lib/pages/home.dart
@@ -10,21 +10,51 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.business),
-            label: 'Business',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.school),
-            label: 'School',
-          ),
-        ],
-    ));
+        appBar: AppBar(backgroundColor: Colors.transparent,
+            //   leading:PopupMenuButton(
+            //   icon: CircleAvatar(
+            //     //   radius: 20,
+            //     //   backgroundImage: Image.asset(
+            //     //           "assets/images/hey.png")
+            // ))),
+            actions: <Widget>[
+              IconButton(
+                  iconSize: 100,
+                  // padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
+                  icon: Image.asset('assets/images/p_image.png'),
+                  onPressed: () {})
+            ]),
+
+        // IconButton(
+        //   icon: const Icon(Icons.navigate_next),
+        //   tooltip: 'Go to the next page',
+        //   onPressed: () {
+        //     Navigator.push(context, MaterialPageRoute<void>(
+        //       builder: (BuildContext context) {
+        //         return Scaffold(
+        //           appBar: AppBar(
+        //             title: const Text('Next page'),
+
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.login),
+              label: 'Home',
+            ),
+            // BottomNavigationBarItem(
+            //   icon: CircleAvatar(
+            //   radius: 20,
+            //   backgroundImage: Image.asset(
+            //           "assets/images/hey.png"),)
+            BottomNavigationBarItem(
+              icon: Icon(Icons.business),
+              label: 'Business',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.school),
+              label: 'School',
+            ),
+          ],
+        ));
   }
 }
