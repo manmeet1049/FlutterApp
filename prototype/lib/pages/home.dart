acf2b5e64108de8faad31c9ddc2aa88607ce3f48
Manmeet <manmeetkohli1049@gmail.com>
2021-11-14 17:07:28 +0530

    bottom Navbar

--- prototype/lib/pages/home.dart
+++ prototype/lib/pages/home.dart
@@ -9,6 +9,22 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('demo')));
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Business',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'School',
+          ),
+        ],
+    ));
   }
 }
