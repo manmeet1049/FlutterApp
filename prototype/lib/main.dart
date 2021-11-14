0f5c3b464f6637c38551d980e389f7824aa95574
Manmeet <manmeetkohli1049@gmail.com>
2021-11-14 16:58:41 +0530

    home page

--- prototype/lib/main.dart
+++ prototype/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'pages/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,10 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Text('Welcome'),
-      themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.pink),
-    );
+    return MaterialApp(home: Home());
   }
 }
